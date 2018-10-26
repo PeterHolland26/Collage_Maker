23767a3bc42947ee896925ef6fb4687c3e275d37
PeterHolland26 <hype2626@hotmail.com>
2018-10-26 09:04:32 +0100

    extra lines added

--- CollageTest.m
+++ CollageTest.m
@@ -3,4 +3,5 @@ disp('MakeCollage')
 %this function will eventually take a directory of images and make a
 %collage of all other images to match a test image as closely as possible
 disp('Hello')
+disp('1')
 end
